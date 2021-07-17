749dccddc792703785706faa36c328734ad0852e
Aavaka77 <sumanthsrao@gmail.com>
2021-07-17 14:58:42 +0530

    Local commit

--- Capstone_Project.py
+++ Capstone_Project.py
@@ -1,4 +1,4 @@
 #this is part of Applied Data science Capstone project week 1 assignment
 import pandas as pd
 import numpy as ny
-print("Hello Capstone Project Course!!")
+print("Hello Capstone Project Course!")
